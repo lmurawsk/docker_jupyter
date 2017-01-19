726cf23b4a891a3a69862fd7fb1c088bc7bc2497
Lukasz Murawski <lmurawsk@gmail.com>
2017-01-19 14:39:51 +0100

    add influxdb

--- start_script.sh
+++ start_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 jupyter notebook --notebook-dir=/opt --ip=* --port=8888 --no-browser &> /dev/null &
-jupyter lab --notebook-dir=/opt --ip=* --port=8889 --no-browser &> /dev/null &
-python /opt/prerequisite/run.py &
+#jupyter lab --notebook-dir=/opt --ip=* --port=8889 --no-browser &> /dev/null &
+#python /opt/prerequisite/run.py &
 /bin/bash -c "while true; do sleep 30; done;"
